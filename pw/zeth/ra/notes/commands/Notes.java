1f7a3678533e05b51f842e52f01c9f8d174100d6
Razeth <34228517+razethion@users.noreply.github.com>
2019-04-20 16:07:39 -0500

    Create Notes.java

---
+++ pw/zeth/ra/notes/commands/Notes.java
@@ -0,0 +1,72 @@
+package pw.zeth.ra.notes.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+
+import net.md_5.bungee.api.ChatColor;
+import pw.zeth.ra.notes.Main;
+
+public class Notes implements CommandExecutor {
+	Main plugin;
+
+	public Notes(Main instance) {
+		plugin = instance;
+	}
+
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+		if(!(sender instanceof Player)) {
+			sender.sendMessage("You must be a player to use this command");
+			return true;
+		}
+
+		Player player = (Player) sender;
+
+		if (args.length == 0) {
+
+			player.sendMessage(ChatColor.RED + "You need to pick a player, dumby!");
+			return true;
+
+		} else {
+
+			//THIS
+			String notee = args[0];
+
+			@SuppressWarnings("deprecation")
+			String uuid = Bukkit.getOfflinePlayer(notee).getUniqueId().toString();
+
+			if (plugin.getConfig().isSet("notes." + uuid)) {
+
+				player.sendMessage(ChatColor.WHITE + "=====" + ChatColor.GOLD + "Displaying " + ChatColor.YELLOW + notee + "'s" + ChatColor.GOLD + " notes" + ChatColor.WHITE + "=====");
+
+				for (String notedtime : plugin.getConfig().getConfigurationSection("notes." + uuid).getKeys(false)) {
+
+					if (!plugin.getConfig().isSet("notes." + uuid + "." +  notedtime + ".deleted") && !plugin.getConfig().isSet("notes." + uuid + "." +  notedtime + ".old")) {
+
+						String notemessage = plugin.getConfig().getString("notes." + uuid + "." + notedtime + ".note");
+						String notedby = plugin.getConfig().getString("notes." + uuid + "." + notedtime + ".noter");
+
+						ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+						String command1 = "tellraw " + player.getName().toString() + " [\"\",{\"text\":\"" + notedtime + " \",\"color\":\"red\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"-\",\"color\":\"gray\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\" \",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"" + notemessage + "\",\"color\":\"white\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"by\",\"color\":\"gray\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\" \",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"" + notedby + "\",\"color\":\"red\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}}]";
+						Bukkit.dispatchCommand(console, command1);
+
+					}
+
+				}
+
+			} else {
+				player.sendMessage(ChatColor.RED + "This player has never been noted.");
+				return true;
+			}
+
+		}
+
+		return true;
+
+	}
+
+}
