067ba763e4a2f418b14499a55d5eeffe766456ed
Razeth <34228517+razethion@users.noreply.github.com>
2019-04-22 11:22:58 -0500

    Update Notes.java

--- pw/zeth/ra/notes/commands/Notes.java
+++ pw/zeth/ra/notes/commands/Notes.java
@@ -39,23 +39,63 @@ public class Notes implements CommandExecutor {
 			@SuppressWarnings("deprecation")
 			String uuid = Bukkit.getOfflinePlayer(notee).getUniqueId().toString();
 
+			@SuppressWarnings("unused")
+			boolean viewdeleted;
+			if(player.hasPermission("notes.viewdeleted")){
+				viewdeleted = true;
+			} else {
+				viewdeleted = false;
+			}
+
 			if (plugin.getConfig().isSet("notes." + uuid)) {
 
 				player.sendMessage(ChatColor.WHITE + "=====" + ChatColor.GOLD + "Displaying " + ChatColor.YELLOW + notee + "'s" + ChatColor.GOLD + " notes" + ChatColor.WHITE + "=====");
 
 				for (String notedtime : plugin.getConfig().getConfigurationSection("notes." + uuid).getKeys(false)) {
 
-					if (!plugin.getConfig().isSet("notes." + uuid + "." +  notedtime + ".deleted") && !plugin.getConfig().isSet("notes." + uuid + "." +  notedtime + ".old")) {
+					if (viewdeleted = false) {
 
-						String notemessage = plugin.getConfig().getString("notes." + uuid + "." + notedtime + ".note");
-						String notedby = plugin.getConfig().getString("notes." + uuid + "." + notedtime + ".noter");
+						if (!plugin.getConfig().isSet("notes." + uuid + "." +  notedtime + ".deleted") && !plugin.getConfig().isSet("notes." + uuid + "." +  notedtime + ".old")) {
 
-						ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
-						String command1 = "tellraw " + player.getName().toString() + " [\"\",{\"text\":\"" + notedtime + " \",\"color\":\"red\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"-\",\"color\":\"gray\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\" \",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"" + notemessage + "\",\"color\":\"white\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"by\",\"color\":\"gray\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\" \",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"" + notedby + "\",\"color\":\"red\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}}]";
-						Bukkit.dispatchCommand(console, command1);
+							String notemessage = plugin.getConfig().getString("notes." + uuid + "." + notedtime + ".note");
+							String notedby = plugin.getConfig().getString("notes." + uuid + "." + notedtime + ".noter");
 
+							ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+							String command1 = "tellraw " + player.getName().toString() + " [\"\",{\"text\":\"" + notedtime + " \",\"color\":\"red\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"-\",\"color\":\"gray\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\" \",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"" + notemessage + "\",\"color\":\"white\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"by\",\"color\":\"gray\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\" \",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"" + notedby + "\",\"color\":\"red\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}}]";
+							Bukkit.dispatchCommand(console, command1);
+
+						}
+					} else {
+
+						if (plugin.getConfig().isSet("notes." + uuid + "." +  notedtime + ".deleted")) {
+							String deletedby = plugin.getConfig().getString("notes." + uuid + "." + notedtime + ".deleted-by").toString();
+							player.sendMessage(deletedby);
+							String isdeleted = " - DELETED BY " + deletedby + " -";
+
+							String notemessage = plugin.getConfig().getString("notes." + uuid + "." + notedtime + ".note");
+							String notedby = plugin.getConfig().getString("notes." + uuid + "." + notedtime + ".noter");
+
+							ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+							String command1 = "tellraw " + player.getName().toString() + " [\"\",{\"text\":\"" + notedtime + isdeleted + " \",\"color\":\"red\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"-\",\"color\":\"gray\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\" \",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"" + notemessage + "\",\"color\":\"white\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"by\",\"color\":\"gray\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\" \",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"" + notedby + "\",\"color\":\"red\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}}]";
+							Bukkit.dispatchCommand(console, command1);
+						} else if (plugin.getConfig().isSet("notes." + uuid + "." +  notedtime + ".old")) {
+							String isold = " - OLD - ";
+
+							String notemessage = plugin.getConfig().getString("notes." + uuid + "." + notedtime + ".note");
+							String notedby = plugin.getConfig().getString("notes." + uuid + "." + notedtime + ".noter");
+
+							ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+							String command1 = "tellraw " + player.getName().toString() + " [\"\",{\"text\":\"" + notedtime + isold + " \",\"color\":\"red\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"-\",\"color\":\"gray\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\" \",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"" + notemessage + "\",\"color\":\"white\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"by\",\"color\":\"gray\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\" \",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"" + notedby + "\",\"color\":\"red\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}}]";
+							Bukkit.dispatchCommand(console, command1);
+						} else {
+							String notemessage = plugin.getConfig().getString("notes." + uuid + "." + notedtime + ".note");
+							String notedby = plugin.getConfig().getString("notes." + uuid + "." + notedtime + ".noter");
+
+							ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+							String command1 = "tellraw " + player.getName().toString() + " [\"\",{\"text\":\"" + notedtime + " \",\"color\":\"red\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"-\",\"color\":\"gray\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\" \",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"" + notemessage + "\",\"color\":\"white\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"by\",\"color\":\"gray\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\" \",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}},{\"text\":\"" + notedby + "\",\"color\":\"red\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/delnote " + notee + " " + notedtime + "\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"Click to delete\"}}]";
+							Bukkit.dispatchCommand(console, command1);
+						}
 					}
-
 				}
 
 			} else {
